8a11b202f614045f9cdbf78cd754b1b28a006c91
gabrielurbani <gabrielurbani@gmail.com>
2022-05-27 21:53:32 -0300

    delete port fron crossorigin

--- src/main/java/com/gurbani/portfolio/controller/ProjectsController.java
+++ src/main/java/com/gurbani/portfolio/controller/ProjectsController.java
@@ -16,7 +16,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "http://192.168.0.20:4200")
+@CrossOrigin(origins = "https://gurbaniportfolio.web.app")
+//@CrossOrigin(origins = "http://192.168.0.20:4200")
 
 public class ProjectsController {
 
