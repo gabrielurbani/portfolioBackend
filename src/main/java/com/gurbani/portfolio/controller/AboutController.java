73774831a80a1699163692e729fcbc2e9c0e7106
gabrielurbani <gabrielurbani@gmail.com>
2022-05-28 23:52:51 -0300

    change about crosorigin

--- src/main/java/com/gurbani/portfolio/controller/AboutController.java
+++ src/main/java/com/gurbani/portfolio/controller/AboutController.java
@@ -15,6 +15,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin(origins = {"https://gurbaniportfolio.web.app"})
+//@CrossOrigin(origins = "http://192.168.0.20:4200")
+
 public class AboutController {
 
     @Autowired
